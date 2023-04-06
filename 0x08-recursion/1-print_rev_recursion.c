7c4298d6ae774637b7de6416be279a77d16b2002
Bundikingson <mwaikingson@gmail.com>
2023-04-06 12:54:27 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
-
 /**
- * _print_rev_recursion - check the code
- *
- * Return: Always 0.
- */
+ * _print_rev_recursion - print a string in reverse
+ * @s: string
+ * Return 0;
+*/
+
 int main(void)
 {
     _print_rev_recursion("\nColton Walker");
