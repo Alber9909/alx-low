eebeb3f115069f366689764125b08d6fc6641789
Bundikingson <Mwaikingson@gmail.com>
2023-07-03 13:11:46 -0700

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,28 +1,33 @@
 #include "lists.h"
 
 /**
- * reverse_listint - the function that reverses a listint_t
- * @head: double pointer
- *
- * Return: a pointer to the first node of the reversed list
+ * find_listint_loop - function to identify a loop
+ * @head: pointer to the head
+ * Return: address of the node where the loop starts, or NULL
  */
-listint_t *reverse_listint(listint_t **head)
+listint_t *find_listint_loop(listint_t *head)
 {
-	listint_t *previous, *next;
+	listint_t *start_point = head;
+	listint_t *end_point = head;
 
-	if (head == NULL || *head == NULL)
+	if (!head)
 		return (NULL);
-	if ((*head)->next == NULL)
-		return (*head);
-	previous = NULL;
-	while (*head != NULL)
-	{
-		next = (*head)->next;
-		(*head)->next = previous;
-		previous = *head;
-		*head = next;
-	}
-	*head = previous;
-	return (*head);
-}
 
+	while (start_point && end_point && end_point->next)
+	{
+		end_point = end_point->next->next;
+		start_point = start_point->next;
+		if (start_point == end_point)
+		{
+			start_point = head;
+			while (start_point != end_point)
+			{
+				start_point = start_point->next;
+				end_point = end_point->next;
+			}
+			return (end_point);
+		}
+	}
+
+	return (NULL);
+}
