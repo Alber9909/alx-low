230c4eadbb7937ec0fa4da9ac82c3e2859a441fd
Alber9909 <alberlish9909@gmail.com>
2023-08-03 14:57:43 -0700

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,60 +1,41 @@
-#include <stdlib.h>
 #include "main.h"
 
-unsigned int _ctoi(char c);
+
 
 /**
- * binary_to_uint - Converts a binary number
- * to an unsigned int.
- * @b: A poiniter to a string of binary chars.
- * Return: The number in base 10 on success,
- * otherwise 0.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
+
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int base, len;
-	unsigned int mul, num;
+	unsigned int ui;
+	int len, base_two;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (*b == '0')
-		b++;
+	ui = 0;
 
-	len = 0;
-	while (b[len])
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if ((b[len] < '0') || (b[len] > '1'))
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		len++;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
 
-	base = 2;
-	num = mul = 0;
-	for (; len > 0; len--)
-	{
-		mul *= base;
-		if (mul == 0)
-			mul = 1;
-		num += (_ctoi(b[len - 1]) * mul);
-	}
-
-	return (num);
-}
-
-/**
- * _ctoi - converts chars to unsigned ints
- * @c: char to convert
- * Return: converted int
- */
-unsigned int _ctoi(char c)
-{
-	unsigned int i;
-	char j;
-
-	if ((c >= '0') || (c <= '9'))
-		for (i = 0, j = '0'; i <= 9; i++, j++)
-			if (c == j)
-				return (i);
-	return (0);
+	return (ui);
 }
